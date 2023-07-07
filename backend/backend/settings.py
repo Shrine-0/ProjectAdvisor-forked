0bc80050c4210153a128e85024b2be18f82c2268
Shrine <nischalthapa10@hotmail.com>
2023-07-07 21:18:59 +0545

    quality of life updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -200,7 +200,7 @@ REST_FRAMEWORK = {
 # ========== Simple JWT's behavior settings ===========
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1,minutes=20),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 
     "AUTH_HEADER_TYPES": ("Bearer",),
